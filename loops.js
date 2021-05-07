2849613487ae3d8a7a50a726fea5e47350e65139
Robert Killion <robertfkillion@gmail.com>
2021-05-07 01:42:13 +0000

    Automatically backed up by Learn

--- loops.js
+++ loops.js
@@ -3,4 +3,11 @@ function forLoop(array) {
   array.push(`I am ${i} strange loop${i === 1 ? '' : 's'}.`); //the readme says it not to add s on 1?
 }
 return array;
+};
+var whileLoop = (n) => {
+  while (n>0) {
+    console.log(n);
+    n--;
+  }
+  return "done";
 }
