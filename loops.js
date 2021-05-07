e1ecb9894f03b9c105296d95efdf12ead1d0dc4a
Robert Killion <robertfkillion@gmail.com>
2021-05-07 01:20:13 +0000

    Automatically backed up by Learn

--- loops.js
+++ loops.js
@@ -1,5 +1,5 @@
 function forLoop(array) {
-  for (let i = 0; i < 25; i++) {
+  for (let i = 1; i < 26; i++) {
   array.push(`I am ${i} strange loop${i === 1 ? '' : 's'}.`); //the readme says it not to add s on 1?
 }
 }
